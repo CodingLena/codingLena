fddfe0f90ef514501355f4660276b95711436d05
CodingLena <lena.feierabend.1992@gmail.com>
2015-09-13 19:01:40 +0200

    PHP<3
    Php-Login

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+$ichbineinname=$_POST['username'];
+$ichbineinpasswort=$_POST['password'];
+
+if($ichbineinname=='Konny' AND $ichbineinpasswort=='123'){
+  echo 'Hallo Konny!';
+}else {
+  echo 'Falscher Name!';
+}
+
+
+?>
