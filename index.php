7ac15f2e13628e3a3d3282590bcc603a9bf947fe
CodingLena <lena.feierabend.1992@gmail.com>
2015-09-14 21:22:27 +0200

    Fixes
    head.html eingebunden, Logo geändert.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<?php require_once('head.html'); ?>
+<body>
+<div class="well">
+<img src="assets/img/cake_header.png" alt="Cake_Header">
+<br>
+<br>
+<input type="text" class="form-control" placeholder="Dein Name..." name="username">
+<br>
+<input type="password" class="form-control" placeholder="Dein Passwort..." name="password">
+<hr>
+<input type="submit" class="btn btn-warning btn-block" value="Login">
+</div>
+</body>
+</html>
