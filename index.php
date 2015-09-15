80ad5f2a7980ba3844ae0592fe358fb9260a2db4
CodingLena <lena.feierabend.1992@gmail.com>
2015-09-15 15:01:13 +0200

    php wiederholung ohne vorlage

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>CodingLena></title>
+<title>CodingLena</title>
 <link rel="stylesheet" href="assets/default/style.css">
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
 <div class="well">
-  <form method="POST" action="login.php">
-  <img src="http://pix.echtlustig.com/d/1411/ich-will-eine-eule_6.jpg" alt="Eulenfoto" class="img-thumbnail">
+<img src="http://pix.echtlustig.com/d/1411/ich-will-eine-eule_6.jpg" alt="Eulenbild" class="img-thumbnail">
 <h2>Willkommen!</h2>
-<input type="text" class="form-control" placeholder="Wer ist hier der Knilch?!" name="username" autofocus>
+<form method="POST" action="login.php">
+<input type="text" class="form-control" placeholder="Dein Name..." name="username" autofocus>
 <br>
 <input type="password" class="form-control" placeholder="Dein Passwort..." name="password">
 <hr>
-<input type="submit" class="btn btn-success btn-block" value="Go!" name="login">
+<input type="submit" class="btn btn-success" value="Login">
+</form>
 </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,11 +1,6 @@
 <?php
 $name=$_POST['username'];
 $password=$_POST['password'];
-if($name=='Konny' AND $password=='123'){
-  echo 'Hallo Konny!';
-} else{ echo'Falscher Name!';
-}
-
-
-
- ?>
+if($name=='Konny' AND $password=='123'){echo 'Hallo!';}
+else{echo 'Falscher Name!';}
+?>
