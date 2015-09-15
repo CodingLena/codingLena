1e2bf79a6284818eee2e8c1e121f70b0317f63a2
CodingLena <lena.feierabend.1992@gmail.com>
2015-09-15 13:57:47 +0200

    PHP Übung
    php wiederholung

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html>
-<?php require_once('head.html'); ?>
+<head>
+  <title>CodingLena></title>
+<link rel="stylesheet" href="assets/default/style.css">
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
 <body>
 <div class="well">
-<img src="assets/img/cake_header.png" alt="Cake_Header">
-<br>
-<br>
-<input type="text" class="form-control" placeholder="Dein Name..." name="username">
+  <form method="POST" action="login.php">
+  <img src="http://pix.echtlustig.com/d/1411/ich-will-eine-eule_6.jpg">
+<h2>Willkommen!</h2>
+<input type="text" class="form-control" placeholder="Wer ist hier der Knilch?!" name="username" autofocus>
 <br>
 <input type="password" class="form-control" placeholder="Dein Passwort..." name="password">
-<hr>
-<input type="submit" class="btn btn-warning btn-block" value="Login">
+<br>
+<input type="submit" class="btn btn-success btn-block" value="Go!" name="login">
 </div>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+$name=$_POST['username'];
+$password=$_POST['password'];
+if($name=='Konny' AND $password=='123'){
+  echo 'Hallo Konny!';
+} else{ echo'Falscher Name!';
+}
+
+
+
+ ?>
