402ef3e108249ad7090f6bbd514f673473296bd5
CodingLena <lena.feierabend.1992@gmail.com>
2015-09-15 14:03:05 +0200

    kleine Verbesserungen
    hr,img-class,,alt

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 <body>
 <div class="well">
   <form method="POST" action="login.php">
-  <img src="http://pix.echtlustig.com/d/1411/ich-will-eine-eule_6.jpg">
+  <img src="http://pix.echtlustig.com/d/1411/ich-will-eine-eule_6.jpg" alt="Eulenfoto" class="img-thumbnail">
 <h2>Willkommen!</h2>
 <input type="text" class="form-control" placeholder="Wer ist hier der Knilch?!" name="username" autofocus>
 <br>
 <input type="password" class="form-control" placeholder="Dein Passwort..." name="password">
-<br>
+<hr>
 <input type="submit" class="btn btn-success btn-block" value="Go!" name="login">
 </div>
 </body>
